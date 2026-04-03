0b89c825c788b235188fd8823f236d6134213a7f
tr1ddlet <dwaekkistan@gmail.com>
2026-04-03 21:43:21 +0400

    routing + nuxt config edits + yml

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,15 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  //devtools: { enabled: true }
+  ssr: false,
+  app: {
+    baseURL: '/napzz/'
+  },
   vite: {
     base: '/napzz/'
+  },
+  router: {
+    options: {
+      hashMode: true
+    }
   }
 })
